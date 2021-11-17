40b3c6412d4bdcf08f23f62750275dadb1b129fa
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-11-17 15:32:49 -0600

    Register Customer function created and working (pending DB FK validations) and errors catched to error_log server file

--- src/config/db/connection.php
+++ src/config/db/connection.php
@@ -12,15 +12,22 @@ class apiDB {
      * Function to stablish the connection to MySQL database
      */
     public function connectToDB() {
-        $mysqlConn = "mysql:host=$this->dbHost;dbname=$this->dbName";// Defines the main connection string
-        $connection = new PDO($mysqlConn, $this->dbUser, $this->dbPassword);// Creates an instance of PDO passing the DB params
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        // Validates if the connection has been established or not
-        if ($connection)
-            return $connection;
-        else 
-            echo "DB connection error. Something went wrong.";
+        try {
+            $mysqlConn = "mysql:host=$this->dbHost;dbname=$this->dbName";// Defines the main connection string
+            $connection = new PDO($mysqlConn, $this->dbUser, $this->dbPassword);// Creates an instance of PDO passing the DB params
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// Attributes setted to capture errors and write it to a log file.
+            
+            // Validates if the connection has been established or not
+            if ($connection)
+                return $connection;
+            else 
+                echo "DB connection error. Something went wrong.";
+        }
+        catch(PDOException $err){
+            error_log('apiDB PDOException - ' . $err->getMessage(), 0);// Sends the error message to the server log file
+            http_response_code(500);// Returns HTTP 500 error and stops the execution...
+            die('Error: Cannot establish connection with database');// ... showing this message
+        }
     }
 
 }
--- src/routes/customers.php
+++ src/routes/customers.php
@@ -27,3 +27,53 @@ $app->get('/customers', function(Request $request, Response $response){
         echo "Error: " . $err->getMessage();
     }
 });
+
+/**
+ * To register a new Customer
+ */
+$app->post('/customers/new', function(Request $request, Response $response){
+    // Puts into variables the data received in request body
+    $dni = $request->getParam('dni');
+    $idReg = $request->getParam('id_reg');
+    $idCom = $request->getParam('id_com');
+    $email = $request->getParam('email');
+    $name = $request->getParam('name');
+    $lastName = $request->getParam('last_name');
+    $address = $request->getParam('address');
+    $dateReg = $request->getParam('date');
+    $dateReg = date("Y-m-d H:i:s", strtotime($dateReg));// Convert received date string into datetime
+    //$status = $request->getParam('status');
+
+    try {
+        // Before execute the query on database, prepare statement by binding parameters (:param)
+        $insertQuery = "INSERT INTO customers (dni, id_reg, id_com, email, name, last_name, address, date_reg) 
+                VALUES (:dni, :id_reg, :id_com, :email, :name, :last_name, :address, :date_reg)";
+        
+        $dbObj = new apiDB();// Creates an instance of DB class
+        $dbObj = $dbObj->connectToDB();
+        $result = $dbObj->prepare($insertQuery);// Prepares the query string to be executed
+        
+        // bindParam function validates user input (API request in this case) before sending it to the DB.
+        $result->bindParam(':dni', $dni);
+        $result->bindParam(':id_reg', $idReg);
+        $result->bindParam(':id_com', $idCom);
+        $result->bindParam(':email', $email);
+        $result->bindParam(':name', $name);
+        $result->bindParam(':last_name', $lastName);
+        $result->bindParam(':address', $address);
+        $result->bindParam(':date_reg', $dateReg);
+        
+        $result->execute();// Executes the validated query on the database;
+
+        echo json_encode("The customer has been saved.");
+        
+        // Resets the variables and close the connection to database
+        $result = null;
+        $dbObj = null;
+    }
+    catch(PDOException $err){
+        error_log('Register Customer PDOException - ' . $err->getMessage(), 0);// Sends the error message to the server log file
+        http_response_code(500);// Returns HTTP 500 error and stops the execution...
+        die('Error: Register process failed. Please report it to Admin.');// ... showing this message
+    }
+});
