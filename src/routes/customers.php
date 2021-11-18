ba504734b856010d59d9bb041a9b1c6f3f0c665f
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-11-17 18:11:00 -0600

    Register Customers function working

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -3,35 +3,14 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+
 $app = new \Slim\App;// Creates new Slim App instance
-
-// ...
-$app->get('/customers', function(Request $request, Response $response){
-    $query = "SELECT * FROM customers;";
-    try {
-        $dbObj = new apiDB();
-        $dbObj = $dbObj->connectToDB();
-        $result = $dbObj->query($query);// Executes query string to bring the data
-
-        if ($result->rowCount() > 0){
-            $customers = $result->fetchAll(PDO::FETCH_OBJ);
-            echo json_encode($customers);
-        }
-        else {
-            echo "There are no registered Customers yet";
-        }
-        $result = null;
-        $dbObj = null;
-    }
-    catch(PDOException $err){
-        echo "Error: " . $err->getMessage();
-    }
-});
 
 /**
  * To register a new Customer
+ * -> POST method
  */
-$app->post('/customers/new', function(Request $request, Response $response){
+$app->post('/customers/register', function(Request $request, Response $response){
     // Puts into variables the data received in request body
     $dni = $request->getParam('dni');
     $idReg = $request->getParam('id_reg');
@@ -92,3 +71,97 @@ $app->post('/customers/new', function(Request $request, Response $response){
         die('Error: Register process failed. Please report it to Admin.');// ... showing this message
     }
 });
+
+
+/**
+ * To search a registered Customer by dni or email
+ * -> POST method
+ */
+$app->post('/customers/search', function(Request $request, Response $response){
+    // Puts into variables the data received in request body
+    $dni = $request->getParam('dni');
+    $email = $request->getParam('email');
+    
+    try {
+        $dbObj = new apiDB();// Creates an instance of DB class
+        $dbObj = $dbObj->connectToDB();// Makes the connection through function
+
+        $selectQuery = "SELECT cus.name, cus.last_name, cus.address, reg.description AS region, com.description AS commune
+                FROM customers cus
+                LEFT JOIN regions reg ON reg.id_reg = cus.id_reg
+                LEFT JOIN communes com ON com.id_com = cus.id_com ";
+
+        if ($dni != "" && $email != ""){
+            $selectQuery .= " WHERE cus.dni = :dni AND cus.email = :email AND cus.status = 'A';";// Before execute the query on database, prepare statement by binding parameters (:param)
+            $result = $dbObj->prepare($selectQuery);// Prepares the query string to be executed
+
+            // bindParam function validates user input (API request in this case) before sending it to the DB.
+            $result->bindParam(':dni', $dni);
+            $result->bindParam(':email', $email);
+
+            $result->execute();// Executes the select query on the database
+
+            if ($result->rowCount() > 0){
+                $customer = $result->fetchAll(PDO::FETCH_OBJ);// Fetch fields and values from obtained resulset
+                echo json_encode($customer);// Returns the requested data
+                echo json_encode(array("success" => "true"));
+            }
+            else {
+                echo json_encode(array("message" => "No customer found.", "success" => "false"));
+            }
+
+            // Resets the variables and close the connection to database
+            $result = null;
+            $dbObj = null;
+        }
+        else if ($dni != "" && $email == ""){
+            $selectQuery .= " WHERE cus.dni = :dni AND cus.status = 'A';";// Before execute the query on database, prepare statement by binding parameters (:param)
+            $result = $dbObj->prepare($selectQuery);// Prepares the query string to be executed
+            $result->bindParam(':dni', $dni);// bindParam function validates user input (API request in this case) before sending it to the DB.
+
+            $result->execute();// Executes the select query on the database
+
+            if ($result->rowCount() > 0){
+                $customer = $result->fetchAll(PDO::FETCH_OBJ);// Fetch fields and values from obtained resulset
+                echo json_encode($customer);// Returns the requested data
+                echo json_encode(array("success" => "true"));
+            }
+            else {
+                echo json_encode(array("message" => "No customer found.", "success" => "false"));
+            }
+
+            // Resets the variables and close the connection to database
+            $result = null;
+            $dbObj = null;
+        }
+        else if ($email != "" && $dni == ""){
+            $selectQuery .= " WHERE cus.email = :email AND cus.status = 'A';";// Before execute the query on database, prepare statement by binding parameters (:param)
+            $result = $dbObj->prepare($selectQuery);// Prepares the query string to be executed
+            $result->bindParam(':email', $email);// bindParam function validates user input (API request in this case) before sending it to the DB.
+
+            $result->execute();// Executes the select query on the database
+
+            if ($result->rowCount() > 0){
+                $customer = $result->fetchAll(PDO::FETCH_OBJ);// Fetch fields and values from obtained resulset
+                echo json_encode($customer);// Returns the requested data
+                echo json_encode(array("success" => "true"));
+            }
+            else {
+                echo json_encode(array("message" => "No customer found.", "success" => "false"));
+            }
+
+            // Resets the variables and close the connection to database
+            $result = null;
+            $dbObj = null;
+        }
+        else {
+            echo "Not enough params to make the Customer search. DNI or email required";
+        }
+    }
+    catch(PDOException $err){
+        error_log('Register Customer PDOException - ' . $err->getMessage(), 0);// Sends the error message to the server log file
+        http_response_code(500);// Returns HTTP 500 error and stops the execution...
+        die('Error: Register process failed. Please report it to Admin.');// ... showing this message
+    }
+});
+
